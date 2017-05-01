c669259b4e4fc0b9475b659eda240c72222e4c84
PrIyanshu-Bhatnagar <priyanshubhatnagar1851996@gmail.com>
2017-05-01 23:34:42 +0530

    Sample message passing

---
+++ Sample message passing/sample_message_passing/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  sample_message_passing
+//
+//  Created by Priyanshu Bhatnagar on 6/18/16.
+//  Copyright © 2016 Priyanshu Bhatnagar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    
+    
+    return 0;
+}
