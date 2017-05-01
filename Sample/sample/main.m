ba1593d09898a9bc8261a9796ecf4b95ec0b36ea
PrIyanshu-Bhatnagar <priyanshubhatnagar1851996@gmail.com>
2017-05-01 23:34:02 +0530

    Sample

---
+++ Sample/sample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  sample
+//
+//  Created by Priyanshu Bhatnagar on 6/17/16.
+//  Copyright © 2016 Priyanshu Bhatnagar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    
+    printf("Hello World\n");
+    
+    return 0;
+}
